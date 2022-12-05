f1f4b6ddf0cdb40ecdd97f5ea6bf468eefc803ba
Mirko Macanovic <mirkomacanovic@yahoo.com>
2022-12-05 23:59:19 +0100

    Before adding kolkov ngx gallery

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  member!: Member;
 
-  constructor() { }
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    let search = this.route.snapshot.paramMap.get('username');
+    if (search !== null) {
+      this.memberService.getMember(search).subscribe({
+        next: (member) => {
+          this.member = member;
+        },
+      });
+    }
+  }
 }
