ad797f0a51147617c691f8e3c5eaa8957afcb3f2
Mirko Macanovic <mirkomacanovic@yahoo.com>
2022-12-06 23:35:20 +0100

    Before adding loading indicators - section 10

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -17,4 +17,8 @@ export class MembersService {
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'Users/' + username);
   }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'Users', member);
+  }
 }
