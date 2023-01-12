0cc62ffef9890cb78a76b02cc0d5e05ed1b2403d
Mirko Macanovic <mirkomacanovic@yahoo.com>
2023-01-12 22:18:03 +0100

    End of Section 19

--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -19,6 +19,7 @@ export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) {}
 
@@ -26,10 +27,12 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading = true;
     this.messageService
       .sendMessage(this.username, this.messageContent)
       .then(() => {
         this.messageForm?.reset();
-      });
+      })
+      .finally(() => (this.loading = false));
   }
 }
